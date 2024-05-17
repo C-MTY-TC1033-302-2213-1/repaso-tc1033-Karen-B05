ca27ef1e73f2684cf3e33edaedc08c645474330b
Karen-B05 <a00840405@tec.mx>
2024-05-16 22:36:45 -0600

    Solución Karen Barrera

---
+++ Punto.hpp
@@ -0,0 +1,31 @@
+#ifndef Punto_hpp
+#define Punto_hpp
+
+#include <stdio.h>
+#include <string>
+#include <math.h>
+
+class Punto{
+private:
+    double x;
+    double y;
+
+public:
+    //Constructores
+    Punto();
+    Punto(double _x,double _y);
+
+    //Métodos de acceso - getters
+    double getX();
+    double getY();
+
+    //Métodos modificadores - setters
+    void setX(double _x);
+    void setY(double _y);
+
+    //Otros metodos
+    double calculaDistancia(Punto p2);
+    std::string str();
+};
+
+#endif // Punto_hpp
